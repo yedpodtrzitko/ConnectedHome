14751751d5db21db59e7924e115d06d4da9b0c35
Eric Mouledoux <EricMouledoux@gmail.com>
2018-03-22 16:08:09 -0500

    Demo TV for demo

---
+++ Assets/Scripts/VideoTextureManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoTextureManager : MonoBehaviour
+{
+    public void PlayVideoTexture()
+    {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+    }
+}
