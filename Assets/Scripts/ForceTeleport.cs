18735eeb8dd1a7b4c2fd766d26a9b45fbb16e938
Eric Mouledoux <EricMouledoux@gmail.com>
2018-03-23 11:15:40 -0500

    Demo went well, art direction approved

--- Assets/Scripts/ForceTeleport.cs
+++ Assets/Scripts/ForceTeleport.cs
@@ -6,20 +6,45 @@ public class ForceTeleport : MonoBehaviour
 {
     public UnityEngine.UI.Image blindfold;
     public float fadeTime;
+    private Vector3 originalPos;
+    private Quaternion originalRot;
 
+    private void Start()
+    {
+        originalPos = Valve.VR.InteractionSystem.Player.instance.transform.position;
+        originalRot = Valve.VR.InteractionSystem.Player.instance.transform.rotation;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Valve.VR.InteractionSystem.Player.instance.transform.position = originalPos;
+            Valve.VR.InteractionSystem.Player.instance.transform.rotation = originalRot;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TeleportPlayerToTransform(transform);
+        }
+    }
 
     public void TeleportPlayerToTransform(Transform pos)
     {
+        Valve.VR.InteractionSystem.Player.instance.gameObject.SetActive(true);
+
         StartCoroutine(FadeInOut());
 
         Valve.VR.InteractionSystem.Player.instance.transform.position = pos.position;
         Valve.VR.InteractionSystem.Player.instance.transform.rotation = pos.rotation;
+
+        StartCoroutine(TimeToTeleportBack());
     }
 
     public IEnumerator FadeInOut()
     {
         Color c = blindfold.color;
-
+        print(blindfold.color);
         c.a = 1f;
         blindfold.color = c;
 
@@ -32,5 +57,13 @@ public class ForceTeleport : MonoBehaviour
 
         c.a = 0f;
         blindfold.color = c;
+    }
+
+    public IEnumerator TimeToTeleportBack()
+    {
+        yield return new WaitForSeconds(10f);
+
+        Valve.VR.InteractionSystem.Player.instance.transform.position = originalPos;
+        Valve.VR.InteractionSystem.Player.instance.transform.rotation = originalRot;
     }
 }
---
+++ Assets/Scripts/UIUtilities.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIUtilities : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex != 0)
+            {
+                LoadLastScene();
+            }
+            else
+            {
+                ExitApp();
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+    }
+
+
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+
+
+    public void ChangeScenes(string aScanaName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(aScanaName);
+    }
+
+
+    public void LoadLastScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+
+    public float CheckScreenSize()
+    {
+        float height = Screen.height;
+        float width = Screen.width;
+        float dpi = Screen.dpi;
+
+        height *= height;
+        width *= width;
+
+        float diag = Mathf.Sqrt(width + height) / dpi;
+
+        return diag;
+    }
+
+
+
+    public void DisableGameobject(GameObject aObject)
+    {
+        aObject.SetActive(false);
+    }
+
+
+
+    public void OpenWebsite(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
