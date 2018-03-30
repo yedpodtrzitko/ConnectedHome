63418df3efaa3bc9225f87221151723a21804ae3
Eric Mouledoux <EricMouledoux@gmail.com>
2018-03-30 12:19:08 -0500

    new blindfold transition test

---
+++ Assets/FireAnimationTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAnimationTrigger : MonoBehaviour
+{
+    Animator m_animator;
+
+	void Start ()
+    {
+        m_animator = GetComponent<Animator>();
+	}
+	
+    public void FireTrigger(string triggerName)
+    {
+        m_animator.SetTrigger(triggerName);
+    }
+}
