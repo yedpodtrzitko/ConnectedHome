203abb80ebd0a8f8354fff5468ddeae8e84c0e56
Eric Mouledoux <EricMouledoux@gmail.com>
2018-04-12 10:26:01 -0500

    Better tunnle end

---
+++ Assets/Scripts/OnTriggerFireEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class OnTriggerFireEvent : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent m_event;
+    public List<string> m_tags;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (m_tags.Count > 0)
+            if (!m_tags.Contains(other.gameObject.tag))
+                return;
+
+        m_event.Invoke();
+    }
+}
