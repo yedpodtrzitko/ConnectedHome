df246818f2990639e5c2ef600f92e17f907e367c
Eric Mouledoux <EricMouledoux@gmail.com>
2018-04-13 14:37:17 -0500

    Headset and wall art

---
+++ Assets/Scripts/VideoSwapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.Video.VideoPlayer))]
+public class VideoSwapper : MonoBehaviour
+{
+    private UnityEngine.Video.VideoPlayer m_videoPlayer;
+    public UnityEngine.Video.VideoClip m_defaultClip;
+
+    public bool m_playOnAwake;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+        m_videoPlayer.clip = m_defaultClip;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+}
