9b382a47ac8d8014bc0222e4fb79f89a91c9af0b
Eric Mouledoux <EricMouledoux@gmail.com>
2018-04-14 17:37:40 -0500

    broadband track doors

---
+++ Assets/Scripts/SacrificeToInvoke.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SacrificeToInvoke : MonoBehaviour
+{
+    public List<GameObject> m_offerings = new List<GameObject>();
+    public UnityEngine.Events.UnityEvent m_action;
+	
+	void FixedUpdate ()
+    {
+		foreach(GameObject go in m_offerings)
+            if (go.activeSelf)
+                return;
+
+        m_action.Invoke();
+
+        m_offerings.Clear();
+        Destroy(this);
+	}
+}
