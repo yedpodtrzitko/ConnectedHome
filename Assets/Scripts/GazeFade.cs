2eadb635417130416f9bad71222fd12a7ba205e1
Eric Mouledoux <EricMouledoux@gmail.com>
2018-04-17 10:01:28 -0500

    TagBubble prefab

---
+++ Assets/Scripts/GazeFade.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeFade : MonoBehaviour
+{
+    [Range(1f, 10f)]
+    public float FadeStrength;
+
+    Renderer m_renderer;
+    Transform m_mainCamera;
+    Material[] m_mats;
+    
+	void Start ()
+    {
+        SetMaterialsToInstances();
+        m_mainCamera = Camera.main.transform;
+	}
+	
+	void Update ()
+    {
+        foreach(Material mat in m_mats)
+        {
+            Color color = mat.color;
+            color.a = 1 - (GetCameraAngle() / 180f) * FadeStrength;
+            mat.color = color;
+        }
+	}
+
+    float GetCameraAngle()
+    {
+        Vector3 angleToCamera = transform.position - m_mainCamera.position;
+        return Vector3.Angle(m_mainCamera.forward, angleToCamera);
+    }
+
+    void SetMaterialsToInstances()
+    {
+        m_renderer = GetComponent<Renderer>();
+
+        m_mats = m_renderer.materials;
+        m_renderer.materials = m_mats;
+    }
+}
