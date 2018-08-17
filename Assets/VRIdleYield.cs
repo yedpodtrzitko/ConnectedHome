6099b10ecf46b3767384a4ad6b5e85d175f09245
Eric Mouledoux <EricMouledoux@gmail.com>
2018-08-17 14:52:02 -0500

    de-commit lighting data

---
+++ Assets/VRIdleYield.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRIdleYield : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent m_onVRActive;
+
+	// Use this for initialization
+	public IEnumerator Start ()
+    {
+        yield return new WaitWhile(() => IdleAutoRestart.isInactive);
+
+        m_onVRActive.Invoke();
+        Destroy(this);
+	}
+}
